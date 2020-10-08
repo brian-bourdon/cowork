efb78647a3a71ac97afdac69dfb578440647c76b
brian-bourdon <brian78.b@gmail.com>
2020-10-08 20:15:54 +0200

    Updates

---
+++ precache-manifest.9904ac20227f5df262364247d82198e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adb6f2e40ad017861cc735130dd0061",
+    "url": "/cowork/index.html"
+  },
+  {
+    "revision": "1ab4440b0233af10a4fb",
+    "url": "/cowork/static/css/2.3eb5a4d0.chunk.css"
+  },
+  {
+    "revision": "b7dd38b18d8c70f5ce24",
+    "url": "/cowork/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ab4440b0233af10a4fb",
+    "url": "/cowork/static/js/2.41baaa4c.chunk.js"
+  },
+  {
+    "revision": "fb09ca5af97b88dba2b2f053ffe1b563",
+    "url": "/cowork/static/js/2.41baaa4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7dd38b18d8c70f5ce24",
+    "url": "/cowork/static/js/main.c94f63b4.chunk.js"
+  },
+  {
+    "revision": "0064c30a89296af2df5c",
+    "url": "/cowork/static/js/runtime-main.f4464972.js"
+  }
+]);
